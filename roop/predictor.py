f6d77452b579d6feef5164ccc791a88ad0ed4bbf
Eskaviam <122778728+eskaviam@users.noreply.github.com>
2023-08-09 11:36:54 +0800

    oh no the table is broken

--- roop/predictor.py
+++ roop/predictor.py
@@ -14,30 +14,23 @@ MAX_PROBABILITY = 0.85
 def get_predictor() -> Model:
     global PREDICTOR
 
-    with THREAD_LOCK:
-        if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    print("hello world")
     return PREDICTOR
 
 
 def clear_predictor() -> None:
     global PREDICTOR
-
+    print("hello world")
     PREDICTOR = None
 
 
 def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
-    return probability > MAX_PROBABILITY
+    print("hello world")
 
 
 def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+    print("hello world")
 
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+    print("hello world")
