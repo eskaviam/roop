94e753acf69e0e2c9fc3dff010641dd8feb76bb7
Eskaviam <122778728+eskaviam@users.noreply.github.com>
2023-08-09 13:34:07 +0800

    W

--- roop-web.py
+++ roop-web.py
@@ -13,6 +13,8 @@ import onnxruntime
 
 from multiprocessing import cpu_count
 import subprocess
+import ngrok
+
 
 import psutil
 import string
@@ -41,7 +43,37 @@ parser.add_argument(
     default = True
 )
 
+parser.add_argument("--ngrok", type=str, help="Use ngrok", default=None)
+parser.add_argument("--ngrok_region", type=str, help="ngrok region", default="us")
+
 args = parser.parse_args()
+
+# Connect to ngrok for ingress
+def connect(token, port, options):
+    account = None
+    if token is None:
+        token = 'None'
+    else:
+        if ':' in token:
+            # token = authtoken:username:password
+            token, username, password = token.split(':', 2)
+            account = f"{username}:{password}"
+
+    # For all options see: https://github.com/ngrok/ngrok-py/blob/main/examples/ngrok-connect-full.py
+    if not options.get('authtoken_from_env'):
+        options['authtoken'] = token
+    if account:
+        options['basic_auth'] = account
+
+
+    try:
+        public_url = ngrok.connect(f"127.0.0.1:{port}", **options).url()
+    except Exception as e:
+        print(f'Invalid ngrok authtoken? ngrok connection aborted due to: {e}\n'
+              f'Your token: {token}, get the right one on https://dashboard.ngrok.com/get-started/your-authtoken')
+    else:
+        print(f'ngrok connected to localhost:{port}! URL: {public_url}\n'
+               'You can use this link after the launch is complete.')
 
 def encode_execution_providers(execution_providers: List[str]) -> List[str]:
     return [execution_provider.replace('ExecutionProvider', '').lower() for execution_provider in execution_providers]
@@ -215,8 +247,8 @@ def start_pl(options_list: tuple, img: str, vid: str, codec: str, cpu_thread_cou
     return '\n'.join(infos), roop.globals.output_path
 
 def GradioInit(UTheme="JohnSmith9982/small_and_pretty"):
-    with gr.Blocks(theme = UTheme, title = "Roop-Web") as app:
-        gr.HTML("<h1>Roop-WebUI-Fork By Alex</h1>")
+    with gr.Blocks(theme = UTheme, title = "Roop UI") as app:
+        gr.HTML("<h1>Roop UI Colab by alunit3</h1>")
         gr.Markdown("sex")
         with gr.Tabs():
             with gr.TabItem(label = "Roop"):
@@ -351,6 +383,9 @@ def GradioInit(UTheme="JohnSmith9982/small_and_pretty"):
                 server_port=7865,
                 quiet=False,
             )
+
+if args.ngrok is not None:
+    connect(args.ngrok, 7865, {'region': args.ngrok_region, 'authtoken_from_env': False})
 
 def preinit():
     if not core.pre_check_nonctk():
